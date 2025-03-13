74193e6eaee55e8df4d47cf7e3ceb554906af675
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2025-03-13 20:40:26 -0300

    Create Ex01.java

---
+++ Exercicios/Ex01.java
@@ -0,0 +1,7 @@
+package Exercicio1;
+
+public class InformacoesPessoais {
+    public static void main(String[] args) {
+        System.out.printf("Nome: %s, Idade: %d anos, Cidade: %s%n", "Maria", 30, "São Paulo");
+    }
+}
