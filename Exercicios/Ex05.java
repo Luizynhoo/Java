5ebedc110ec67dfe3b90edd758e271a2aa84437f
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2025-03-13 20:41:43 -0300

    Create Ex05.java

---
+++ Exercicios/Ex05.java
@@ -0,0 +1,22 @@
+package Exercicio5;
+
+import java.util.Scanner;
+
+public class InformacoesProduto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o nome do produto: ");
+        String produto = sc.nextLine();
+
+        System.out.println("Digite o preço do produto: ");
+        double preco = sc.nextDouble();
+
+        System.out.println("Digite a quantidade em estoque: ");
+        int quantidade = sc.nextInt();
+
+        System.out.printf("Produto: %s | Preço: R$ %.2f | Quantidade: %d unidades%n", produto, preco, quantidade);
+
+        sc.close();
+    }
+}
