ccec5b56fbddeb4d6fc8ea74f745b8528ecdb4b8
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2025-05-01 20:35:37 -0300

    Create Ex03.java

---
+++ Listas/Lista06/Ex03.java
@@ -0,0 +1,17 @@
+// Dado um vetor v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, criar um programa que imprima
+// estes valores na tela
+
+package Listas;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		int[] v = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int num : v) {
+            System.out.println(num);
+        }
+
+	}
+
+}
